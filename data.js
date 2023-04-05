d99dea2ae786d713f4317e98039688c0356949aa
Ben Megidish <megidish46@gmail.com>
2023-04-05 17:10:05 +0300

    commit

--- data.js
+++ data.js
@@ -1,10 +1,14 @@
 
-const { Builder,By } = require('selenium-webdriver')
+const { Builder, By } = require('selenium-webdriver')
 const utiles = require('./telegram');
 const helpers = require('./helpers/help');
-
+const chrome = require('selenium-webdriver/chrome');
 const searchData = async (city) => {
-    let driver = new Builder().forBrowser("chrome").build();
+
+
+    const service = new chrome.ServiceBuilder();
+    const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
+    //let driver = new Builder().forBrowser("chrome").build();
     driver.manage().window().maximize();
     await driver.get(`https://www.rest.co.il/kosher-restaurants/${city}/kosher/`);
     let res = (await driver.findElement(By.className('restaurant-info-top')).getText()).trim();
@@ -15,7 +19,7 @@ const searchData = async (city) => {
     let counter = 0
     while (counter < num) {
         driver.manage().window().maximize();
-        await driver.get('https://www.rest.co.il/kosher-restaurants/' + city + '/kosher/page-'+page);
+        await driver.get('https://www.rest.co.il/kosher-restaurants/' + city + '/kosher/page-' + page);
         let places = await driver.findElements(By.css(".rest-title"));
         for (let place of places) {
             counter += 1
@@ -23,9 +27,9 @@ const searchData = async (city) => {
             console.log(await place.getText() + " number: " + counter)
             utiles.SendMessage(await place.getText())
         }
-        page+=1
+        page += 1
     }
     driver.quit();
 };
 
-module.exports={searchData};
+module.exports = { searchData };
